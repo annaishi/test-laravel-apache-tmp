1c065118dea6be359bb74a77de10281045dbad99
annaishi <annaishilala@gmail.com>
2025-06-10 19:51:33 +0900

    vueアプリケーション作成(vue,vue-router,pinia)

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+import "./bootstrap";
+
+import { createApp } from "vue";
+import { router } from "./router.js";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+
+createApp(App).use(router).use(createPinia()).mount("#app");
